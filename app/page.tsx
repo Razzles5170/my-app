321e82da785fe7ef14c824850a4d9ffb3ac7131e
Razzles5170 <22AhamedR845@collyers.ac.uk>
2024-09-16 16:41:55 +0100

    added better spacing between text things and also added a letter pullup for the long sentence but will be changed to a word pullup soon

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import WordRotate from "@/components/magicui/word-rotate";
 import MarqueeComponent from "@/components/magicui/marquee";
 import HyperText from "@/components/magicui/hyper-text";
 import AnimatedGradient from "@/components/magicui/animated-gradient-text";
+import LetterPullup from "@/components/magicui/letter-pullup"; 
 
 function Home() {
   return (
@@ -14,11 +15,11 @@ function Home() {
       <Navbar />
 
       {/* Main Content */}
-      <div className="flex flex-col justify-center items-center min-h-screen bg-white">
+      <div className="flex flex-col justify-center items-center min-h-screen pt-5 bg-white">
+      <div className="flex flex-col items-center space-y-8">
       <AnimatedGradient className="text-5xl font-extrabold text-black-mb4 bg-gradient-to-r from-[#c0c0c0] via-[#a0a0a0] to-[#c0c0c0] bg-clip-text text-transparent animate-gradient">
         Welcome to CoderDojo
       </AnimatedGradient>
-      <ChevronRight className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
 
       
 
@@ -27,8 +28,8 @@ function Home() {
           className="text-6xl font-bold text-black mb-8"
         />
 
-        <HyperText
-          text="Join a global movement of free, volunteer-led coding clubs for young people!"
+        <LetterPullup
+          words="Join a global movement of free, volunteer-led coding clubs for young people!"
           className="text-2xl text-black mb-8 text-center"
         />
 
@@ -40,6 +41,7 @@ function Home() {
           </div>
         </MarqueeComponent>
       </div>
+    </div>
     </div>
   );
 }
